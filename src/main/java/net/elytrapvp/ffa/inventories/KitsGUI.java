6f59dbd6b47a460f925d0b860d23aa181e46b112
colby <colbykrawczyk10@gmail.com>
2021-12-27 00:22:30 -0500

    Update KitsGUI format

--- src/main/java/net/elytrapvp/ffa/inventories/KitsGUI.java
+++ src/main/java/net/elytrapvp/ffa/inventories/KitsGUI.java
@@ -16,7 +16,7 @@ import java.util.Arrays;
 import java.util.List;
 
 public class KitsGUI extends CustomGUI {
-    private static final int[] slots = new int[]{11,12,13,14,15,20,21,22,23,24,29,30,31,32,33};
+    private static final int[] slots = new int[]{10,11,12,13,14,15,16,19,20,21,22,23,24,25,28,29,30,31,32,33,34};
 
     public KitsGUI(Player p) {
         super(54, "Kits");
@@ -35,12 +35,12 @@ public class KitsGUI extends CustomGUI {
                     if(cp.getUnlockedKits().contains(kit.getId())) {
                         player.closeInventory();
                         cp.setKit(kit.getId());
-                        ChatUtils.chat(player, "&2&lKit &8- &aYou have selected Kit &f" + kit.getName() + "&a.");
+                        ChatUtils.chat(player, "&a&lKit &8» &aYou have selected Kit &f" + kit.getName() + "&a.");
                     }
                     else {
                         if(cp.getCoins() >= kit.getPrice()) {
                             cp.removeCoins(kit.getPrice());
-                            ChatUtils.chat(player, "&2&lKit &8- &aYou have purchased and equipped Kit &f" + kit.getName() + "&a.");
+                            ChatUtils.chat(player, "&a&lKit &8» &aYou have purchased and equipped Kit &f" + kit.getName() + "&a.");
                             cp.setKit(kit.getId());
                             cp.unlockKit(kit.getId());
                             p.closeInventory();
@@ -57,12 +57,12 @@ public class KitsGUI extends CustomGUI {
 
         setItem(40, new ItemBuilder(Material.COMPASS).setDisplayName("&aSpectator Mode").build(), (player, action) -> {
             cp.setKit(-1);
-            ChatUtils.chat(p, "&2&lSpectator &8- &aEntered Spectator Mode");
+            ChatUtils.chat(p, "&a&lSpectator &8» &aEntered Spectator Mode");
         });
     }
 
     private void fillers() {
-        List<Integer> slots = Arrays.asList(0,1,2,3,4,5,6,7,8,9,10,16,17,18,19,25,26,27,28,34,35,36,37,43,44,45,46,47,48,49,50,51,52,53);
+        List<Integer> slots = Arrays.asList(0,1,2,3,4,5,6,7,8,45,46,47,48,49,50,51,52,53);
         ItemStack filler = new ItemBuilder(Material.GRAY_STAINED_GLASS_PANE).setDisplayName(" ").build();
         slots.forEach(i -> setItem(i, filler));
     }
