06d21001ce31236b7c4f0246c39b699152cb1d13
colby <colbykrawczyk10@gmail.com>
2021-12-27 22:10:21 -0500

    Update bomber kit

--- src/main/java/net/elytrapvp/ffa/objects/kits/BomberKit.java
+++ src/main/java/net/elytrapvp/ffa/objects/kits/BomberKit.java
@@ -7,30 +7,39 @@ import net.elytrapvp.ffa.objects.Kit;
 import org.bukkit.Color;
 import org.bukkit.FireworkEffect;
 import org.bukkit.Material;
-import org.bukkit.enchantments.Enchantment;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.potion.PotionEffect;
+import org.bukkit.potion.PotionEffectType;
 
 public class BomberKit extends Kit {
     public BomberKit() {
         super("Bomber", 7);
 
-        ItemStack bow = new ItemBuilder(Material.BOW)
-                .setDisplayName("&aBomber Bow")
+        ItemStack crossbow = new ItemBuilder(Material.CROSSBOW)
+                .setDisplayName("&aBomber Crossbow")
                 .setUnbreakable(true)
-                .addEnchantment(Enchantment.ARROW_DAMAGE, 3)
-                .addEnchantment(Enchantment.ARROW_INFINITE, 1)
                 .build();
-        addItem(0, bow);
+        addItem(0, crossbow);
+        addItem(1, crossbow);
+        addItem(2, crossbow);
 
         FireworkEffect effect = FireworkEffect.builder()
                 .withColor(Color.RED)
+                .with(FireworkEffect.Type.BALL_LARGE)
                 .build();
+
         ItemStack fireworks = new FireworkBuilder()
                 .addEffect(effect)
+                .setPower(3)
                 .setDisplayName("&aExplosive Fireworks")
+                .setAmount(64)
                 .build();
-        addItem(2, fireworks);
+        addItem(40, fireworks);
+        addItem(3, new ItemBuilder(Material.FIREWORK_ROCKET, 64).build());
+
+        PotionEffect slowFalling = new PotionEffect(PotionEffectType.SLOW_FALLING, 9999, 0);
+        addEffect(slowFalling);
 
         setPrice(400);
     }
